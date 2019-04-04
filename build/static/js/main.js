bb46d064dcc754b8bb66686ab66bdd0f026fd94d
Ivan Bochkarev <ivanx86@gmail.com>
2019-04-05 05:52:17 +0600

    Add slider reasons

--- build/static/js/main.js
+++ build/static/js/main.js
@@ -1,46 +1,17 @@
 $(document).ready(function () {
 
-  $('.slider_wrap').slick({
+  $('#slider-post .slider_wrap').slick({
     slidesToShow: 3,
-    slidesToScroll: 3,
+    slidesToScroll: 1,
     dots: true,
     arrows: false
   });
 
-  allCarousels.each(function () {
-    var slidesRow = $(this).find('.slider_wrap'),
-      slideControls = $(this).find('.slide-controls');
-
-    if (slidesRow.is('.slider_wrap')) {
-      slidesRow.slick({
-        slide: '.slider_item',
-        autoplay: true,
-        appendArrows: slideControls,
-        prevArrow: slideControls.find('.slide-arrow.arrow-prev'),
-        nextArrow: slideControls.find('.slide-arrow.arrow-next'),
-        appendDots: slideControls,
-        dots: true,
-        slidesToShow: 3,
-        slidesToScroll: 3,
-        dotsClass: 'custom-dots', //slick generates this <ul.custom-dots> within the appendDots container
-        customPaging: function (slider, i) {
-          var slideNumber = i + 1,
-            totalSlides = slider.slideCount;
-          return (
-            '<a class="dot" role="button" title="' +
-            slideNumber +
-            ' of ' +
-            totalSlides +
-            '"><span class="string">' +
-            slideNumber +
-            '/' +
-            totalSlides +
-            '</span></a>'
-          );
-        }
-      });
-    }
+  $('#slider-reasons .slider_wrap').slick({
+    slidesToShow: 4,
+    slidesToScroll: 4,
+    dots: true,
+    arrows: false
   });
-
 
 });
