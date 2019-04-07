98da6f7dd01d3248a55196327b8c4329ffb0fd3f
Ivan Bochkarev <ivanx86@gmail.com>
2019-04-07 18:37:51 +0600

    Add adaptive styles for phone

--- build/static/js/main.js
+++ build/static/js/main.js
@@ -4,21 +4,93 @@ $(document).ready(function () {
     slidesToShow: 3,
     slidesToScroll: 1,
     dots: true,
-    arrows: false
+    arrows: false,
+    responsive: [{
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+    ]
   });
 
   $('#slider-reasons .slider_wrap').slick({
     slidesToShow: 4,
     slidesToScroll: 4,
     dots: true,
-    arrows: false
+    arrows: false,
+    responsive: [{
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+    ]
   });
 
   $('#clients .slider_wrap').slick({
     slidesToShow: 6,
     slidesToScroll: 2,
     dots: true,
-    arrows: false
+    arrows: false,
+    responsive: [{
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2
+        }
+      }
+    ]
   });
 
 });
